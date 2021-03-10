f2e6cc5fd1c5498aead4261f4da78f585caf35ba
Kalena Singbandith <kalenaisn00b@gmail.com>
2021-03-10 15:37:02 -0800

    created login page

--- app/src/main/java/com/ksing/plantpartner/MainActivity.java
+++ app/src/main/java/com/ksing/plantpartner/MainActivity.java
@@ -13,11 +13,26 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
+    public Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Creates Login Button to direct to Login Page
+        button = (Button) findViewById(R.id.loginButton);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            //Opens Login Page
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Creates Button to direct to Quiz
         Button quizBtn = findViewById(R.id.quizButton);
         quizBtn.setOnClickListener(new View.OnClickListener(){
 
